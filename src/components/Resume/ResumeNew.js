c4cab305b2bdc63bb2906b83c3e0192e6fd14df1
Ashish <ashish1311998@gmail.com>
2022-05-06 01:14:15 +0530

    First Commit

---
+++ src/components/Resume/ResumeNew.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import { Container, Row } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import Particle from "../Particle";
+import pdf from "../../Assets/../Assets/Ashish-Resume.pdf";
+import { AiOutlineDownload } from "react-icons/ai";
+import { Document, Page, pdfjs } from "react-pdf";
+import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
+
+const resumeLink =
+  "https://drive.google.com/file/d/1vy_pC3UqfhFjfEBZZqAng3GJ-UeBvBLZ/view?usp=sharing";
+
+function ResumeNew() {
+  const [width, setWidth] = useState(1200);
+
+  useEffect(() => {
+    setWidth(window.innerWidth);
+  }, []);
+
+  return (
+    <div>
+      <Container fluid className="resume-section">
+        <Particle />
+        
+
+        <Row className="resume">
+          <Document file={resumeLink} className="d-flex justify-content-center">
+            <Page pageNumber={1} scale={width > 786 ? 1.7 : 0.6} />
+          </Document>
+        </Row>
+
+        <Row style={{ justifyContent: "center", position: "relative" }}>
+          <Button
+            variant="primary"
+            href={pdf}
+            target="_blank"
+            style={{ maxWidth: "250px" }}
+          >
+            <AiOutlineDownload />
+            &nbsp;Download CV
+          </Button>
+        </Row>
+        <h3 className="Info">Still Working on to Show my Resume on this page as well.</h3>
+      </Container>
+    </div>
+  );
+}
+
+export default ResumeNew;
